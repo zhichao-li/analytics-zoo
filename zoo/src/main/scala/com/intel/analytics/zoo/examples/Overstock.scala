21716ddb03d182ebe65b14589454013dccb47684
zhichao-li <zhichao.li@intel.com>
2018-06-13 15:38:28 +0800

    update

--- zoo/src/main/scala/com/intel/analytics/zoo/examples/Overstock.scala
+++ zoo/src/main/scala/com/intel/analytics/zoo/examples/Overstock.scala
@@ -18,15 +18,18 @@ class Overstock {
 
 }
 
+// we need a evaluate method for debug purpose.
 object Overstock {
 
   def cLoss(yTrue: Variable[Float], yPred: Variable[Float])(
       implicit ev: TensorNumeric[Float]): Variable[Float] = {
-    val truePositive = Merge[Float](mode = "dot").from[Float](yTrue, yPred)
+    val binaryPred = BinaryThreshold[Float](0.5).from(yPred)
+    val truePositive = A.sum(yTrue * binaryPred)
+    //val truePositive = Merge[Float](mode = "dot").from[Float](yTrue, binaryPred)
     truePositive
 //    A.mean(A.abs(yTrue - yPred), axis = 1)
   }
-  val customLoss = CustomLoss[Float](cLoss _)
+  val customLoss = CustomLoss[Float](cLoss _, sizeAverage = false)
 
   def lr(): KerasNet[Float] = {
     val seq = Sequential[Float]()
@@ -41,9 +44,9 @@ object Overstock {
 //  }
 
   def main(args: Array[String]): Unit = {
-    val yTrue = Tensor[Float](data = Array(0f, 1f, 0f, 1f), shape = Array(4, 1))
-    val yPred = Tensor[Float](data = Array(0.3f, 0.6f, 0.7f, 0.2f), shape = Array(4, 1))
-    val l = customLoss.forward(yTrue, yPred)
+    val yTrue = Tensor[Float](data = Array(0f, 1f, 1f, 1f, 0f), shape = Array(5, 1))
+    val yPred = Tensor[Float](data = Array(0.3f, 0.6f, 0.7f, 0.2f, 0.8f), shape = Array(5, 1))
+    val l = customLoss.forward(yPred, yTrue) // NOTE: the order is yPred then yTrue in forward
     println(l)
   }
 }
