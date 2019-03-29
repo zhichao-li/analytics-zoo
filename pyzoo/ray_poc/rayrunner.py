2c57b4847ca31883f9a2307f158c81e6c42a361b
zhichao-li <zhichao.li@intel.com>
2019-03-29 15:14:33 +0800

    commit kill

--- pyzoo/ray_poc/rayrunner.py
+++ pyzoo/ray_poc/rayrunner.py
@@ -4,10 +4,21 @@ import threading
 from pyspark import BarrierTaskContext
 import atexit
 import re
+import signal
+import sys
+import itertools
 
 
-from ray_poc.util.process import session_execute
+from ray_poc.util.process import session_execute, ProcessResource
 
+
+def _gen_shutdown_per_node(pgids):
+    def _shutdown_per_node(iter):
+        print("shutting down pgid: {}".format(pgids))
+        for pgid in pgids:
+            print("killing {}".format(pgid))
+            os.killpg(pgid, signal.SIGTERM)
+    return _shutdown_per_node
 
 class RayContext(object):
     """
@@ -69,16 +80,19 @@ class RayContext(object):
         command = "{} start --redis-address {} --redis-password  {} --num-cpus {}".format(
             self.ray_exec, redis_address, self.password, self.slave_cores)
         print("".format(command))
+
         modified_env = self.prepare_env(self.master_cores)
         session_execute(command, env=modified_env)
         time.sleep(self.WAITING_TIME_SEC)
 
     def stop_ray(self):
-        command="{} stop".format(self.ray_exec)
-        print("cleaning the ray cluster: {}".format(command))
-        modified_env = self.prepare_env(self.master_cores)
-        session_execute(command, env=modified_env)
-        time.sleep(self.WAITING_TIME_SEC)
+        # it would have issue if two ray process running on the same node.
+        pass
+        # command="{} stop".format(self.ray_exec)
+        # print("cleaning the ray cluster: {}".format(command))
+        # modified_env = self.prepare_env(self.master_cores)
+        # session_execute(command, env=modified_env)
+        # time.sleep(self.WAITING_TIME_SEC)
 
     def get_ray_exec(self):
         python_bin_dir = "/".join(self.python_loc.split("/")[:-1])
@@ -104,10 +118,11 @@ class RayContext(object):
             if tc.partitionId() == 0:
                 print("partition id is : {}".format(tc.partitionId()))
                 self.start_master()
-                yield redis_address
+                yield redis_address, ProcessResource.pgids
             else:
                 print("partition id is : {}".format(tc.partitionId()))
                 self.start_raylet(redis_address=redis_address)
+                yield None, ProcessResource.pgids
             tc.barrier()
 
         return _start_ray_services
@@ -144,23 +159,59 @@ class RayRunner(object):
 
     def run(self):
         # TODO: before involving thread, we need to figure out how to return the redis_address to user
-        # ray_launching_thread = threading.Thread(target=self._run)
+
+        # ray_launching_thread = threading.Thread(target=self._run, args=(self.result_queue, ))
         # ray_launching_thread.start()
+        # redis_address = self.result_queue.get()
+        # return redis_address
         return self._run()
 
+    def _parse_running_meta(self, running_meta):
+        addrs = []
+        pgids = []
+        for pmeta in running_meta:
+            addrs.append(pmeta[0])
+            pgids.append(pmeta[1])
+        addrs = [addr for addr in addrs if addr]
+        pgids=list(itertools.chain.from_iterable(pgids))
+        assert len(addrs) == 1, "we should only have one redis address"
+        return addrs[0], pgids
+
     def _run(self):
-        redis_address = self.sc.range(0,
+        # TODO: it should return [IP, gpid], then we can only kill the gpid if the ip address match.
+        running_meta = self.sc.range(0,
                                       self.num_executors,
                                       numSlices=self.num_executors).barrier().mapPartitions(
             self.ray_context.gen_ray_booter()).collect()
+        redis_address, pgids = self._parse_running_meta(running_meta)
         self.redis_address = redis_address
+        ProcessResource.pgids = pgids
+        # what if half of the services is started and then exception happen?
+        self.register_shutdown_hook()
         return redis_address
 
-    def stop(self):
-        self.sc.range(0,
-                      self.num_executors + 1,
-                      numSlices=self.num_executors + 1).barrier().mapPartitions(
-            self.ray_context.stop_ray()).collect()
+
+    def register_shutdown_hook(self):
+        def _shutdown():
+            self.sc.range(0,
+                          self.num_executors,
+                          numSlices=self.num_executors).map(_gen_shutdown_per_node(ProcessResource.pgids)).collect()
+
+        def _signal_shutdown(_signo, _stack_frame):
+            self._shutdown()
+            sys.exit(0)
+
+        # TODO: are there any other signal we want to handle?
+        atexit.register(_shutdown)
+        signal.signal(signal.SIGTERM, _signal_shutdown)
+        signal.signal(signal.SIGINT, _signal_shutdown)
+
+
+    # def stop(self):
+    #     self.sc.range(0,
+    #                   self.num_executors + 1,
+    #                   numSlices=self.num_executors + 1).barrier().mapPartitions(
+    #         self.ray_context.stop_ray()).collect()
 
     def start_driver(self):
         """
--- pyzoo/ray_poc/util/process.py
+++ pyzoo/ray_poc/util/process.py
@@ -3,6 +3,10 @@ import subprocess
 import signal
 import atexit
 
+class ProcessResource(object):
+    pgids=[]
+
+
 def session_execute(command, env=None):
     pro = subprocess.Popen(
         command,
@@ -13,19 +17,16 @@ def session_execute(command, env=None):
         stderr=subprocess.PIPE,
         preexec_fn=os.setsid)
     pgid = os.getpgid(pro.pid)
+    ProcessResource.pgids.append(pgid)
+    print("---------------" + str(pgid))
     out, err = pro.communicate()
+    out=out.decode("utf-8")
+    err=err.decode("utf-8")  # converting bytes to string otherwise \n would not be recognized using str(err)
     errcode = pro.returncode
-    def _shutdown():
-        os.killpg(pgid, signal.SIGTERM)
-    # TODO: are there any other signal we want to handle?
-    atexit.register(_shutdown)
-    signal.signal(signal.SIGTERM, _shutdown)
-    signal.signal(signal.SIGINT, _shutdown)
     if errcode != 0:
         # https://bip.weizmann.ac.il/course/python/PyMOTW/PyMOTW/docs/atexit/index.html
         # http://www.pybloggers.com/2016/02/how-to-always-execute-exit-functions-in-python/    register for signal handling
         raise Exception(err)
     else:
-        print(err)
         print(out)
     return out, err
