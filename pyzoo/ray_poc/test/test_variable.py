14c64032e0bbf52d9a15a4e4ef5de23a4e6a6cc5
zhichao-li <zhichao.li@intel.com>
2019-04-15 10:43:29 +0800

    update variable

--- pyzoo/ray_poc/test/test_variable.py
+++ pyzoo/ray_poc/test/test_variable.py
@@ -1,11 +1,20 @@
 
 import tensorflow as tf
+import numpy as np
 
-x = tf.Variable(4)
+v = tf.Variable(np.ones([4]), dtype=tf.int32)
 # we can feed data into Variables
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    print(x.eval()) # print 4
-    print(sess.run(x, feed_dict={x: 15})) # print 15
+    print(v.eval()) # print 4
+    print(sess.run(v, feed_dict={v: np.ones([4]) + 5})) # print 15
+    print(v.eval()) # if without assign and feed the data directly, the value of variable would not be changed !!!!
+    p=tf.placeholder(
+        tf.int32,
+        [4],
+        name="Placeholder_a" )
+    y_assigned = v.assign(p)
+    print(sess.run(y_assigned, feed_dict={p: np.ones([4]) + 5})) # print 15
+    print(v.eval(sess))
 
 
