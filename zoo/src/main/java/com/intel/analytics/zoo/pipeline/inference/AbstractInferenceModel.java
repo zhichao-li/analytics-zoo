7fc56f5cf1d6645155128a430bbcd4c30dc1ff77
zhichao-li <zhichao.li@intel.com>
2019-06-25 04:38:19 +0000

    spark2.4.1

--- zoo/src/main/java/com/intel/analytics/zoo/pipeline/inference/AbstractInferenceModel.java
+++ zoo/src/main/java/com/intel/analytics/zoo/pipeline/inference/AbstractInferenceModel.java
@@ -16,10 +16,9 @@
 
 package com.intel.analytics.zoo.pipeline.inference;
 
-import scala.actors.threadpool.Arrays;
-
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public abstract class AbstractInferenceModel extends InferenceModel implements Serializable {
@@ -119,11 +118,11 @@ public abstract class AbstractInferenceModel extends InferenceModel implements S
   }
 
   public List<List<JTensor>> predict(List<JTensor>[] inputs) {
-    return predict(Arrays.asList(inputs));
+    return predict(java.util.Arrays.asList(inputs));
   }
 
   public List<List<JTensor>> predictInt8(List<JTensor>[] inputs) {
-    return predictInt8(Arrays.asList(inputs));
+    return predictInt8(java.util.Arrays.asList(inputs));
   }
 
   @Override
